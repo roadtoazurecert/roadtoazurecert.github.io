5637791aa694291c98d99a8f30a96e3a14aef3b2
Douglas Starnes <douglas.a.starnes@gmail.com>
2023-05-16 01:51:29 -0500

    Added base

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,6 +30,7 @@ if (isBuild) {
 export default defineConfig({
   server: { port: SERVER_PORT },
   site: "https://roadtoazurecert.github.io",
+  base: "/astrosrc",
   integrations: [
     sitemap(),
     tailwind({
